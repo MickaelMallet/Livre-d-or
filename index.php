285ce3fa054e80e2d4dabcdaa231c53ac6123de2
MickaelMallet <113337099+MickaelMallet@users.noreply.github.com>
2024-01-12 11:54:35 +0100

    logique taille pseudo/formulaire; class Message

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php 
+require_once 'class/Message.php';
+$errors = null;
 if (isset($_POST['username'], $_POST['message'])) {
-    
+    $message = new Message($_POST['username'], $_POST['message']);
+    if ($message->isValid()) {
+        // Traitement si le message est valide
+    } else {
+        $errors = 'Formulaire invalide';
+    }
 }
 $title = "Livre d'or";
 require 'elements/header.php';
@@ -8,15 +15,22 @@ require 'elements/header.php';
 
 <div class="container">
     <h1>Livre d'or</h1>
+
+    <?php if ($errors): ?>
+        <div class="alert alert-danger">
+            <?= $errors ?>
+        </div>
+    <?php endif; ?>
+
     <form action="" method="post">
         <div class="form-group">
             <input type="text" name="username" placeholder="Votre pseudo">
         </div>
         <div class="form-group">
-            <textarea name="username" placeholder="Votre pseudo"></textarea>
+            <textarea name="message" placeholder="Votre message"></textarea>
         </div>
         <button class="btn btn-primary">Envoyer</button>
     </form>
 </div>
 
-<?php require 'elements/footer.php';
+<?php require 'elements/footer.php'; ?>
