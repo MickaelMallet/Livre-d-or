3d6832bb5e54a63bf81c44b97cdb86a1ad8da658
MickaelMallet <113337099+MickaelMallet@users.noreply.github.com>
2024-01-12 12:19:28 +0100

    gestion des erreurs

--- class/Message.php
+++ class/Message.php
@@ -1,6 +1,8 @@
 <?php 
 class Message {
 
+    const LIMIT_USERNAME= 3;
+    const LIMIT_MESSAGE= 10;
     private $username;
     private $message;
 
@@ -12,6 +14,19 @@ class Message {
 
     public function isValid(): bool 
     {
-        return strlen($this->username) >= 3 && strlen($this->message) <= 10;
+        return empty($this->getErrors());
     }
+    
+    public function getErrors(): array 
+    {
+        $errors = [];
+        if (strlen($this->username) < self::LIMIT_USERNAME) {
+            $errors['username'] = "Votre pseudo est trop court";
+        }
+        if (strlen($this->message) < self::LIMIT_MESSAGE) {
+            $errors['message'] = "Votre message est trop court";
+        }
+        return $errors;
+    }
+    
 }
--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php 
 require_once 'class/Message.php';
 $errors = null;
+
 if (isset($_POST['username'], $_POST['message'])) {
     $message = new Message($_POST['username'], $_POST['message']);
-    if ($message->isValid()) {
-        // Traitement si le message est valide
-    } else {
-        $errors = 'Formulaire invalide';
-    }
+    $errors = $message->getErrors();
 }
+
 $title = "Livre d'or";
 require 'elements/header.php';
 ?>
@@ -16,18 +14,24 @@ require 'elements/header.php';
 <div class="container">
     <h1>Livre d'or</h1>
 
-    <?php if ($errors): ?>
+    <?php if (!empty($errors)): ?>
         <div class="alert alert-danger">
-            <?= $errors ?>
+            Formulaire invalide
         </div>
     <?php endif; ?>
 
     <form action="" method="post">
         <div class="form-group">
-            <input type="text" name="username" placeholder="Votre pseudo">
+            <input type="text" name="username" placeholder="Votre pseudo" class="form-control <?= isset($errors['username']) ? 'is-invalid' : ''; ?>">
+            <?php if (isset($errors['username'])): ?>
+                <div class="invalid-feedback"><?= $errors['username'] ?></div>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <textarea name="message" placeholder="Votre message"></textarea>
+            <textarea name="message" placeholder="Votre message" class="form-control <?= isset($errors['message']) ? 'is-invalid' : ''; ?>"></textarea>
+            <?php if (isset($errors['message'])): ?>
+                <div class="invalid-feedback"><?= $errors['message'] ?></div>
+            <?php endif; ?>
         </div>
         <button class="btn btn-primary">Envoyer</button>
     </form>
